7e37e0c5e188f37e2dca8d56dfa183f86c7450ab
Adolfo <allorenter@gmail.com>
2022-09-06 19:34:55 +0200

    Instalo react-query

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchMovies from '@/pages/SearchMovies';
 import Movie from '@/pages/Movie';
 import MyList from '@/pages/MyList';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-      <BrowserRouter>
-        <Routes>
-          <Route path='movie/:movieId' element={<Movie />} />
-          <Route path='mylist' element={<MyList />} />
-          <Route path='' element={<SearchMovies />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='movie/:movieId' element={<Movie />} />
+            <Route path='mylist' element={<MyList />} />
+            <Route path='' element={<SearchMovies />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
