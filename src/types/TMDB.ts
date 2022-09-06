732855e187aafefd3041e64f93c781f59569cfb5
Adolfo <allorenter@gmail.com>
2022-09-06 19:09:12 +0200

    Defino correctamente los tipos en Movie

--- src/types/TMDB.ts
+++ src/types/TMDB.ts
@@ -184,38 +184,3 @@ export interface Translations {
   id: number;
   translations: Translation[];
 }
-
-export interface TMDBMovie {
-  adult: boolean;
-  backdrop_path: string;
-  belongs_to_collection?: any;
-  budget: number;
-  genres: Genre[];
-  homepage: string;
-  id: number;
-  imdb_id: string;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path?: any;
-  production_companies: ProductionCompany[];
-  production_countries: ProductionCountry[];
-  release_date: string;
-  revenue: number;
-  runtime: number;
-  spoken_languages: SpokenLanguage[];
-  status: string;
-  tagline: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
-
-export interface TMDBSearch<T> {
-  page: number;
-  results: T[];
-  total_pages: number;
-  total_results: number;
-}
