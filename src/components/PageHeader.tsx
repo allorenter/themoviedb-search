f1a4a9f1c709f00586a62ba58c33c4e38dc26903
Adolfo <allorenter@gmail.com>
2022-09-07 21:34:42 +0200

    Volver a main

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,5 +1,21 @@
+import ArrowLeft from '@/icons/ArrowLeft';
+import { Title } from '@mantine/core';
+
 function PageHeader({ text, showGoBackButton }: { text: string; showGoBackButton: boolean }) {
-  return <h3>{text}</h3>;
+  return (
+    <Title
+      sx={() => ({
+        display: 'flex',
+        alignItems: 'center',
+        gap: 6,
+      })}
+      order={3}
+      mb={12}
+    >
+      {showGoBackButton && <ArrowLeft />}
+      {text}
+    </Title>
+  );
 }
 
 export default PageHeader;
