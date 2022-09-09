15e0083d79aff81ecb249d31fbe5481e2b02f606
Adolfo <allorenter@gmail.com>
2022-09-09 21:46:39 +0200

    Rutas lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { Suspense, useState, lazy } from 'react';
 import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
-import { theme } from '@/theme';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SearchMovies from '@/pages/SearchMovies';
-import Movie from '@/pages/Movie';
-import MyList from '@/pages/MyList';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import Layout from '@/components/Layout';
 import { ProvideSearchMoviesTerm } from './global-states/useSearchMoviesTerm';
 import { NotificationsProvider } from '@mantine/notifications';
 import { ProvideSearchMoviesPage } from './global-states/useSarchMoviesPage';
+
+const SearchMovies = lazy(() => import('@/pages/SearchMovies'));
+const Movie = lazy(() => import('@/pages/Movie'));
+const MyList = lazy(() => import('@/pages/MyList'));
 
 const queryClient = new QueryClient();
 
@@ -21,26 +21,28 @@ export default function App() {
   };
 
   return (
-    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-        <NotificationsProvider>
-          <QueryClientProvider client={queryClient}>
-            <ProvideSearchMoviesTerm>
-              <ProvideSearchMoviesPage>
-                <BrowserRouter>
-                  <Layout>
-                    <Routes>
-                      <Route path='movie/:movieId' element={<Movie />} />
-                      <Route path='mylist' element={<MyList />} />
-                      <Route path='' element={<SearchMovies />} />
-                    </Routes>
-                  </Layout>
-                </BrowserRouter>
-              </ProvideSearchMoviesPage>
-            </ProvideSearchMoviesTerm>
-          </QueryClientProvider>
-        </NotificationsProvider>
-      </MantineProvider>
-    </ColorSchemeProvider>
+    <Suspense fallback={<></>}>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+          <NotificationsProvider>
+            <QueryClientProvider client={queryClient}>
+              <ProvideSearchMoviesTerm>
+                <ProvideSearchMoviesPage>
+                  <BrowserRouter>
+                    <Layout>
+                      <Routes>
+                        <Route path='movie/:movieId' element={<Movie />} />
+                        <Route path='mylist' element={<MyList />} />
+                        <Route path='' element={<SearchMovies />} />
+                      </Routes>
+                    </Layout>
+                  </BrowserRouter>
+                </ProvideSearchMoviesPage>
+              </ProvideSearchMoviesTerm>
+            </QueryClientProvider>
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </Suspense>
   );
 }
