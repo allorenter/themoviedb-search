9d8f5a1a6d98d1765202455d2ab39d4b8fce9fe4
Adolfo <allorenter@gmail.com>
2022-09-11 19:15:27 +0200

    Añado loader

--- src/pages/MyList/index.tsx
+++ src/pages/MyList/index.tsx
@@ -1,5 +1,5 @@
 import PageHeader from '@/components/PageHeader';
-import { Stack } from '@mantine/core';
+import { Center, Loader, Space, Stack } from '@mantine/core';
 import RatedMovieItem from './componentes/RatedMovieItem';
 import useGetRatedMovies from './hooks/useGetRatedMovies';
 
@@ -9,11 +9,18 @@ function MyList() {
   return (
     <>
       <PageHeader title='Mi Lista' showGoBackButton={true} />
-      <Stack mt={24}>
-        {data?.map((ratedMovie) => (
-          <RatedMovieItem key={ratedMovie.id} ratedMovie={ratedMovie} />
-        ))}
-      </Stack>
+      <Space h='lg' />
+      {isFetching && isLoading ? (
+        <Center mt={12}>
+          <Loader color={'gray'} />
+        </Center>
+      ) : (
+        <Stack>
+          {data?.map((ratedMovie) => (
+            <RatedMovieItem key={ratedMovie.id} ratedMovie={ratedMovie} />
+          ))}
+        </Stack>
+      )}
     </>
   );
 }
