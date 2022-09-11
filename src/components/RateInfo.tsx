a0cc04f71428ba646675c589bb557b9cc582bb56
Adolfo <allorenter@gmail.com>
2022-09-11 16:37:59 +0200

    Obtengo colores con un array en lugar de un objeto

--- src/components/RateInfo.tsx
+++ src/components/RateInfo.tsx
@@ -1,25 +1,25 @@
 import { Text } from '@mantine/core';
 
-const COLORS = {
-  '0': '#c90000',
-  '1': '#c12b00',
-  '2': '#b95200',
-  '3': '#b17600',
-  '4': '#a99600',
-  '5': '#8fa000',
-  '6': '#669800',
-  '7': '#409000',
-  '8': '#1e8800',
-  '9': '#1b8700',
-  '10': '#008000',
-};
+const COLORS = [
+  '#c90000',
+  '#c12b00',
+  '#b95200',
+  '#b17600',
+  '#a99600',
+  '#8fa000',
+  '#669800',
+  '#409000',
+  '#1e8800',
+  '#1b8700',
+  '#008000',
+];
 
 function RateInfo({ rate }: { rate?: number }) {
   if (!rate) {
     return <></>;
   }
 
-  const color: string = COLORS[Math.round(rate).toString()];
+  const color: string = COLORS[Math.round(rate)];
 
   const formattedRate = rate.toFixed(1);
 
